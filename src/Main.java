179e818c171ba55713acb08671ada6f233bc1b9d
Максим Евсеев <vailogiant@gmail.com>
2023-04-13 23:56:08 +0500

    Созданы классы для книг и авторов. Созданы объекты авторов и книг

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        Author harlanEllison = new Author("Харлан", "Эллисон");
+        Author nikolayGogol = new Author("Николай", "Гоголь");
+        Author philipReeve = new Author("Филип", "Рив");
 
+        Book dangerousVisions = new Book("Опасные видения", harlanEllison, 1967);
+        Book tarasBulba = new Book("Тарас Бульба", nikolayGogol, 1835);
+        Book mortalEngines = new Book("Смертные машины", philipReeve, 2001);
+
+        tarasBulba.setPublicationYear(2003);
+
+        Book[] bookShelf = {dangerousVisions, tarasBulba, mortalEngines};
+
+        for (int i = 0; i < bookShelf.length; i++) {
+            System.out.println(bookShelf[i].getName() + ", " + bookShelf[i].getAuthor().getName() + " "
+                    + bookShelf[i].getAuthor().getSurname() + ", " + bookShelf[i].getPublicationYear() + "г.");
+        }
     }
 }
